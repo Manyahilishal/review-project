f91bfeacacd6ff6cd276b8c94f95582f010bc06f
Manny <manyaabat@gmail.com>
2022-03-28 16:33:08 -0400

    first commit to script file

--- script.js
+++ script.js
@@ -16,3 +16,12 @@ let buttonHandler = () => {
 genBtn.addEventListener("click", buttonHandler);
 
 // ========= ⬆ DO NOT TOUCH THIS CODE ⬆ ======
+var password=document.getElementById("password");
+var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ var passwordLength = 12;
+ var password = "";
+ for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+   }
+   document.getElementById("password").value = password;
