a14c44e31fc0cb6ab06bd1c7e29f1982c952b765
Manny <manyaabat@gmail.com>
2022-03-29 22:25:01 -0400

    edited again

--- script.js
+++ script.js
@@ -7,25 +7,14 @@
 // Using the above array and password length variable, create a random password using a for loop inside of a function called "addNewPassword" either saved as an arrow function variable or a traditional function
 
 // ========= ⬇ DO NOT TOUCH THIS CODE ⬇ ======
-function password_generator( len ) {
-  var length = (len)?(len):(10);
-  var string = "abcdefghijklmnopqrstuvwxyz"; //to upper 
-  var numeric = '0123456789';
-  var punctuation = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
-  var password = "";
-  var character = "";
-  var crunch = true;
-  while( password.length<length ) {
-      entity1 = Math.ceil(string.length * Math.random()*Math.random());
-      entity2 = Math.ceil(numeric.length * Math.random()*Math.random());
-      entity3 = Math.ceil(punctuation.length * Math.random()*Math.random());
-      hold = string.charAt( entity1 );
-      hold = (password.length%2==0)?(hold.toUpperCase()):(hold);
-      character += hold;
-      character += numeric.charAt( entity2 );
-      character += punctuation.charAt( entity3 );
-      password = character;
+function makePasswd() {
+  var passwd = '';
+  var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  for (i=1;i<8;i++) {
+    var c = Math.floor(Math.random()*chars.length + 1);
+    passwd += chars.charAt(c)
   }
-  password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
-  return password.substr(0,len);
+
+  return passwd;
+
 }
